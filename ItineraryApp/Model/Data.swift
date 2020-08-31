7e2f814ea18430ed2f09ec64f126c273971283a9
Media Davarkhah <Media.dvh@gmail.com>
2020-08-31 21:48:17 +0430

    Added readtrips code

---
+++ ItineraryApp/Model/Data.swift
@@ -0,0 +1,12 @@
+//
+//  Data.swift
+//  ItineraryApp
+//
+//  Created by Media Davarkhah on 6/10/1399 AP.
+//  Copyright © 1399 Media Davarkhah. All rights reserved.
+//
+
+import Foundation
+class Data{
+    static var tripModels = [TripModel]()
+}
